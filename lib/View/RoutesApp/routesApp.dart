12a90d0ed521b810a269bf8222c4182b45d8c416
Ahmed Shaker <amhmeed31@gmail.com>
2024-10-23 22:56:56 +0300

    add sha

---
+++ lib/View/RoutesApp/routesApp.dart
@@ -0,0 +1,9 @@
+import 'package:clothing_store/View/HomeScreen/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+// Map<String, Widget Function(BuildContext)> routes = {
+//   '/': (context) => const HomeScreen(),
+//   // '/login': (context) => const LoginScreen(),
+//   // '/register': (context) => const RegisterScreen(),
+//   // '/forgotpassword': (context) => const ForgotPasswordScreen(),
+// };
